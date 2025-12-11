ffdccaa7866c7d3dae022f0f93591091caa3f52a
OumouMohamedBa <24408@esp.mr>
2025-12-11 17:26:54 +0100

    Update header.component.ts

--- Frontend/src/app/header/header.component.ts
+++ Frontend/src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
 import { CommonModule, UpperCasePipe } from '@angular/common';
 
+
 import { AuthService } from '../services/auth.service'; 
 
 @Component({
